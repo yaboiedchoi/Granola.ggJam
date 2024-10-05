1e7e1648911c515f5a80363ed2ad828ac391abfd
Jason Henderson (RIT Student) <jah1026@rit.edu>
2024-10-05 15:07:38 -0400

    DinoGame Progress
    Missing background and obstacle sprites.

--- GranolaGame/Assets/Scripts/Minigames/DinoGame/obstacle.cs
+++ GranolaGame/Assets/Scripts/Minigames/DinoGame/obstacle.cs
@@ -21,7 +21,6 @@ public class obstacle : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("NextLine"))
         {
-            Debug.Log("running");
             obstacleGenerator.RandomizeSpace();
         }
     }
