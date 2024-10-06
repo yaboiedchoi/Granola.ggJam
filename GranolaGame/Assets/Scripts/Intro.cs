b0b00a00f9503df93911f6a26fda92600928cbca
rlawson04 <rbl2991@rit.edu>
2024-10-06 00:24:36 -0400

    Main Menu Working

--- GranolaGame/Assets/Scripts/Intro.cs
+++ GranolaGame/Assets/Scripts/Intro.cs
@@ -10,11 +10,15 @@ public class Intro : MonoBehaviour
     [SerializeField] Button playButton;
     [SerializeField] Button creditButton;
     [SerializeField] Button optionsButton;
+    [SerializeField] Canvas menuCanvas;
+    [SerializeField] Canvas creditsCanvas;
+    [SerializeField] Canvas optionsCanvas;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        creditsCanvas.enabled = false;
+        optionsCanvas.enabled = false;
     }
 
     // Update is called once per frame
@@ -26,5 +30,29 @@ public class Intro : MonoBehaviour
     public void StartGame()
     {
         SceneManager.LoadScene("Game Scene");
+    }
+
+    public void OpenCredits()
+    {
+        creditsCanvas.enabled = true;
+        menuCanvas.enabled = false;
+    }
+
+    public void CloseCredits()
+    {
+        creditsCanvas.enabled = false;
+        menuCanvas.enabled = true;
+    }
+
+    public void OpenOptions()
+    {
+        optionsCanvas.enabled = true;
+        menuCanvas.enabled = false;
+    }
+
+    public void CloseOptions() 
+    {
+        optionsCanvas.enabled = false;
+        menuCanvas.enabled = true;
     }
 }
