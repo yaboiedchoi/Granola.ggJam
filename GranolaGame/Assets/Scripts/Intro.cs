c5a45bc0c1f48b82b2216f1e6ea134998416b3d2
yaboiedchoi <ec2244@rit.edu>
2024-10-06 00:52:14 -0400

    Revert "Merge branch 'main' of https://github.com/yaboiedchoi/Granola.ggJam"
    This reverts commit 69a0c700471f6fab9ea8e2ed14dd35950082264c, reversing
    changes made to aded2c0f4c62c3b1cd60aab1d94a69fc802b49aa.

--- GranolaGame/Assets/Scripts/Intro.cs
+++ GranolaGame/Assets/Scripts/Intro.cs
@@ -13,15 +13,12 @@ public class Intro : MonoBehaviour
     [SerializeField] Canvas menuCanvas;
     [SerializeField] Canvas creditsCanvas;
     [SerializeField] Canvas optionsCanvas;
-    [SerializeField] Slider sfxVol;
-    [SerializeField] Slider musicVol;
 
     // Start is called before the first frame update
     void Start()
     {
         creditsCanvas.enabled = false;
         optionsCanvas.enabled = false;
-        
     }
 
     // Update is called once per frame
@@ -32,8 +29,6 @@ public class Intro : MonoBehaviour
 
     public void StartGame()
     {
-        PlayerPrefs.SetFloat("SFX Volume", sfxVol.value);
-        PlayerPrefs.SetFloat("Music Volume", musicVol.value);
         SceneManager.LoadScene("Game Scene");
     }
 
